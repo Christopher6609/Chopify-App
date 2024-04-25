e899565d8d2432a98b302c2ea1b4ba2bd642b683
Christopher <odeniyichristopher7@gmail.com>
2024-04-25 23:45:36 +0100

    chore: bug during deployment fixed

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Landing } from "../pages";
 
 const router = createBrowserRouter([{ path: "/", element: <Landing /> }]);
